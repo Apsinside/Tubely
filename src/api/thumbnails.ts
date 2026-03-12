52aef7c934da1b5f6f74c51ba929914b2231eba4
Fabian <f.zickwolff@googlemail.com>
2026-03-12 15:06:24 +0100

    CH1: File Storage

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,40 +1,10 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import path from "path";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -47,7 +17,55 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if(!(file instanceof File)){
+    throw new BadRequestError("Uploaded image is not a valid file");
+  }
 
-  return respondWithJSON(200, null);
+  const MAX_UPLOAD_SIZE = 10 << 20;
+  if(file.size > MAX_UPLOAD_SIZE ){
+    throw new BadRequestError("Maximum file size of 10MB exceeded");
+  }
+
+  const mediaType = file.type;
+  const data = await file.arrayBuffer();
+  
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+
+  if(video?.userID !== userID){
+    throw new UserForbiddenError("Video does not belong to user");
+  }
+
+  const fileExtension = getFileExtension(mediaType);
+  const filePath = path.join(cfg.assetsRoot, videoId) + "." + fileExtension;
+  const bytesWritten = await Bun.write(filePath, data);
+  // if(bytesWritten !== file.size){
+  //   throw new Error(`Failed to store video thumbnail file at ${filePath}`)
+  // }
+  console.log("Uploaded thumbnail for video", videoId, "stored at", filePath);
+
+  video.thumbnailURL = `http://localhost:8091/assets/${videoId}.${fileExtension}`;
+
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
+}
+
+
+function getFileExtension(mediaType: string): string{
+  switch(mediaType){
+    case ("image/png"):
+      return "png";
+    case ("application/json"):
+      return "json";
+    case ("text/html"):
+      return "html";
+    default:
+      return "";
+  }
+
 }
