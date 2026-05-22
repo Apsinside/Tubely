2b553bb78ec2c4eba208ae4f3641eed020434cb1
Fabian <f.zickwolff@googlemail.com>
2026-05-22 15:32:07 +0200

    CH3: AWS S3 | L3: Dynamic Path

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -9,6 +9,8 @@ import path from "path";
 import { randomBytes } from "crypto";
 import { createGunzip } from "zlib";
 import { getTsBuildInfoEmitOutputFilePath } from "typescript";
+import { json } from "stream/consumers";
+import { log } from "console";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -56,16 +58,56 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const tempFilePath = path.join(cfg.assetsRoot, filePath);
 
   await Bun.write(tempFilePath, data);
+  const aspectRatio = await getVideoAspectRatio(tempFilePath);
 
-  const s3File = cfg.s3Client.file(filePath, { type: mediaType });
+  const s3FilePath = `${aspectRatio}/${filePath}`
+  const s3File = cfg.s3Client.file(s3FilePath, { type: mediaType });
+
   await s3File.write(Bun.file(tempFilePath));
-
   await Bun.file(tempFilePath).delete()
 
-  video.videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${filePath}`;
+  video.videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${s3FilePath}`;
   console.log("Uploaded video", videoId, "stored at", video.videoURL);
 
   updateVideo(cfg.db, video);
 
   return respondWithJSON(200, null);
+}
+
+
+async function getVideoAspectRatio(filePath: string): Promise<string> {
+  const proc = Bun.spawn({
+    cmd: ["ffprobe",
+      "-v",
+      "error",
+      "-select_streams",
+      "v:0",
+      "-show_entries",
+      "stream=width,height",
+      "-of",
+      "json",
+      filePath],
+    stdout: "pipe",
+    stderr: "pipe"
+  });
+  const exitCode = await proc.exited;
+  const stdoutText = await new Response(proc.stdout).text();
+  const stderrText = await new Response(proc.stderr).text();
+  if (exitCode !== 0) {
+    throw new BadRequestError("ffprobe failed with " + stderrText);
+  }
+
+  const json = await JSON.parse(stdoutText);
+  const width = json["streams"][0]["width"];
+  const height = json["streams"][0]["height"];
+
+  const aspectRatio = Math.floor(width / height);
+
+  if (aspectRatio === 1) {
+    return "landscape";;
+  } else if (aspectRatio < 1) {
+    return "portrait";
+  }
+
+  return "other";
 }
